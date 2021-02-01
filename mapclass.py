94be982c64c65d0d1b3703db794c274965e6ebe3
Error273 <cfif12349@ya.ru>
2021-02-01 16:32:54 +0500

    2

--- mapclass.py
+++ mapclass.py
@@ -3,12 +3,21 @@ from io import BytesIO
 
 
 class YandexMap:
-    def __init__(self):
+    def __init__(self, centercoords, scale):
         self.api_server = 'http://static-maps.yandex.ru/1.x/'
+        self.centercoords = centercoords
+        self.scale = scale
 
-    def get_map(self, coords, scale):
-        params = {'ll': ','.join([str(i) for i in coords]),
-                 'spn': ','.join([str(i) for i in scale]),
+
+    def get_map(self):
+        params = {'ll': ','.join([str(i) for i in self.centercoords]),
+                 'spn': ','.join([str(i) for i in self.scale]),
                  'l': 'map'}
         response = requests.get(self.api_server, params=params)
         return BytesIO(response.content)
+
+    def set_centercoords(self, centercoords):
+        self.centercoords = centercoords
+
+    def set_scale(self, scale):
+        self.scale = scale
