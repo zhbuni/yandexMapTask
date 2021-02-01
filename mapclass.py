fdf1ae547c74e3936148834bf919bde80d44cd1c
Error273 <cfif12349@ya.ru>
2021-02-01 17:25:14 +0500

    ddf

--- mapclass.py
+++ mapclass.py
@@ -7,19 +7,55 @@ class YandexMap:
         self.api_server = 'http://static-maps.yandex.ru/1.x/'
         self.centercoords = centercoords
         self.scale = scale
+        self.type = 'map'
+        self.points = []
 
 
     def get_map(self):
         params = {'ll': ','.join([str(i) for i in self.centercoords]),
                  'spn': ','.join([str(i) for i in self.scale]),
-                 'l': 'map',
-                  'size':'800,800'}
+                 'l': self.type}
+
+        for i in self.points:
+            point = ','.join([str(j) for j in i]) + ',org'
+            if 'pt' not in params:
+                params['pt'] = point
+            else:
+                params['pt'] += '~' + point
 
         response = requests.get(self.api_server, params=params)
-        return BytesIO(response.content)
+        return response.content
 
     def set_centercoords(self, centercoords):
         self.centercoords = centercoords
 
     def set_scale(self, scale):
         self.scale = scale
+
+    def change_type(self, type):
+        self.type = type
+
+    def find_object(self, toponym_to_find):
+        geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
+
+        geocoder_params = {
+            "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
+            "geocode": toponym_to_find,
+            "format": "json"}
+
+        response = requests.get(geocoder_api_server, params=geocoder_params)
+
+        # Преобразуем ответ в json-объект
+        json_response = response.json()
+        # Получаем первый топоним из ответа геокодера.
+        toponym = json_response["response"]["GeoObjectCollection"][
+            "featureMember"][0]["GeoObject"]
+
+        # Координаты центра топонима:
+        toponym_coodrinates = toponym["Point"]["pos"]
+
+        return toponym_coodrinates
+
+    def add_point(self, coords):
+        self.points.append(coords)
+
